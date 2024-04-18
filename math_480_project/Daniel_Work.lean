99b1266cf19b7952f939ee5454d7a4a53f46d557
Daniel <Dan814@uw.edu>
2024-04-18 14:41:42 -0700

    Added possibly useful definitions

--- math_480_project/Daniel_Work.lean
+++ math_480_project/Daniel_Work.lean
@@ -9,3 +9,9 @@ example: {n : ℤ | 12∣ n} ⊆ {n : ℤ | 3∣n} := by
   rcases h with ⟨k, hk⟩
   use 4*k
   linarith
+
+def A : Finset ℕ := {1,2,3,4}
+def PA := A.powerset
+#eval PA
+#check (Finset.range 10)
+#eval Finset.range 10
