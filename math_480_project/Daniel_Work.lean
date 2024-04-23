38ca1009ec5f92924d7f7ef9267a73d0de443dc7
Daniel <Dan814@uw.edu>
2024-04-23 13:12:43 -0700

    Continued working on Prop 3.19 + added small example to work with

--- math_480_project/Daniel_Work.lean
+++ math_480_project/Daniel_Work.lean
@@ -4,7 +4,7 @@ import Mathlib.Algebra.BigOperators.Basic
 import Mathlib.Data.Nat.GCD.Basic
 open BigOperators
 open Finset
-
+open Function
 
 example: {n : ℤ | 12∣ n} ⊆ {n : ℤ | 3∣n} := by
   intro x
@@ -15,10 +15,22 @@ example: {n : ℤ | 12∣ n} ⊆ {n : ℤ | 3∣n} := by
   use 4*k
   linarith
 
-def A : Finset ℕ := {1,2,3,4}
-def PA := A.powerset
-#eval PA
+def B : Finset ℕ := {1,2,3,4}
+def PB := B.powerset
+#eval PB
 #check (Finset.range 10)
 #eval Finset.range 10
+#eval B
 
-example: ∀A ⊆ {1, 2, 3, 4, 5}, card A = 10 → ∃ X,Y ⊆ A ∧ X ≠ Y ∧ ∑ x in X, x = ∑ y in Y, y := by sorry
+example: ∀A ⊆ (Finset.range 101), card A = 10 → ∃ X,Y ⊆ A ∧ X ≠ Y ∧ ∑ x in X, x = ∑ y in Y, y := by sorry
+#eval Finset.range 101
+#check Finset.range 101
+#check {Finset.range 101}
+#check {0}
+#check 0
+#eval {0,1}
+#eval 0
+
+-- Smaller Example to work through for practice on syntax and how to apply pigeonhole principle
+-- Task: Formalize that f(x)=x^2 is not injective using the pigeonhole principle
+example: (α: Type ℤ) (β: Type ℤ) (f: α → β) fun x ↦ x^2 : ¬ Injective fun x ↦ x^2 := by sorry
