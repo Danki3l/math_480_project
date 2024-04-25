96af9d5a2471dfeacd1cb1c51d0defe75f2faf4c
Daniel <Dan814@uw.edu>
2024-04-25 13:01:32 -0700

    Proved that g(x) = x^2 is not injective "straightforwardly"; continued synax practice

--- math_480_project/Daniel_Work.lean
+++ math_480_project/Daniel_Work.lean
@@ -28,9 +28,35 @@ example: ∀A ⊆ (Finset.range 101), card A = 10 → ∃ X,Y ⊆ A ∧ X ≠ Y 
 #check {Finset.range 101}
 #check {0}
 #check 0
-#eval {0,1}
+-- #eval {0,1}
 #eval 0
 
 -- Smaller Example to work through for practice on syntax and how to apply pigeonhole principle
 -- Task: Formalize that f(x)=x^2 is not injective using the pigeonhole principle
-example: (α: Type ℤ) (β: Type ℤ) (f: α → β) fun x ↦ x^2 : ¬ Injective fun x ↦ x^2 := by sorry
+
+-- Straight-forward proof using the fact that g(-1) = g(1) but 1 ≠ -1
+def g (x : ℤ) := x^2
+example: ¬ Injective g := by
+intro h
+rw[Injective] at h
+specialize @h 1 (-1)
+simp [g] at h
+-- rw[g, g] at h
+
+--Prove the same result using the pigeonhole principle:
+example: ¬ Injective g := by sorry
+
+
+-- Components to Understand
+-- 1. Understanding how to make a function from integers to integers, specifically f(x) = x^2
+-- 2. Undestanding how to make finite sets s = {1, -2, 2} and t = {1, 4}
+-- 3. How to show that ∀x ∈ s, f(x) ∈ t
+
+-- 1:
+variable (f : ℤ → ℤ)
+#check g (-1)
+#eval g (-2)
+
+-- 2:
+def s : Finset ℤ  := {1, -2, 2}
+def t: Finset ℤ := {1, 4}
